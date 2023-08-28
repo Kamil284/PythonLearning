f06b7853b4ca6111d19a29caad3bfb79f2691e7d
Kamil284 <kkowalik5197@gmial.com>
2023-08-28 22:01:47 +0200

    Edit code

--- Cenapaliwa1.py
+++ Cenapaliwa1.py
@@ -4,23 +4,33 @@ def ilosc_osob(km,sp,cp,os): ##km=ilosc kilometrow sp=spalanie na 100km cp=cena 
     return (km / sp * cp) / os
 
 while(True):
-    print("Wybierz: ")
-    print('"1" jeżeli jedziesz sam')
-    print('"2" jeżeli jedziesz z kimś')
+    print('"Wybierz:\n"1" jeżeli jedziesz sam!\n"2" jeżeli jedziesz z kimś!')
     wybor = input("Wprowadz liczbe: ")
     if wybor == "1":
         km = float(input("Wprowadz ilość kilometrów do przejechania: "))
         sp = float(input("Wprowadz spalanie na 100km: "))
         cp = float(input("Wprowadz cene paliwa za jeden litr w PLN: "))
         print("Kosz przejchania tej trasy wynosi", round(koszt_drogi(km, sp, cp), 2))
-        break
+        next = input("Czy chcesz wprowadzić dane jeszcze raz? TAK / NIE \n")
+        if next.upper() == "TAK":
+            print("Okej więc jeszcze raz!")
+            continue
+        elif next.upper() == "NIE":
+            print("Miłej i bezpiecznej drogi!")
+            break
     elif wybor == "2":
         km = float(input("Wprowadz ilość kilometrów do przejechania: "))
         sp = float(input("Wprowadz spalanie na 100km: "))
         cp = float(input("Wprowadz cene paliwa za jeden litr w PLN: "))
         os = float(input("Wprowadz liczbe osób w samochodzie: "))
         print("Kosz przejchania tej trasy dla", int(os), "osób wynosi", round(ilosc_osob(km, sp, cp, os),2), "na osobe")
-        break
+        next = input("Czy chcesz wprowadzić dane jeszcze raz? TAK / NIE \n")
+        if next.upper() == "TAK":
+            print("Okej więc jeszcze raz!")
+            continue
+        elif next.upper() == "NIE":
+            print("Miłej i bezpiecznej drogi!")
+            break
     else:
         print("Nie wybrałeś poprawnej opcji.\n" "Spróbuj ponownie!\n")
         continue

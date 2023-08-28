a842dde1ab895b75cceb0cb9b51a3f508d0368ed
Kamil <124830972+Kamil284@users.noreply.github.com>
2023-08-28 21:18:15 +0200

    Add files via upload

---
+++ Petle.py
@@ -0,0 +1,10 @@
+#Dopoki, powtarzanie wiecej razy
+
+#taki zapis dodaje poprzedni wynik do kolejnej liczby
+
+suma = 0
+while True:
+    print("wpisz liczbe ")
+    x = input()
+    suma += int(x)
+    print(suma)

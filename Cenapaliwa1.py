2cc47f2f8566b9f54148e5a728eff86ae697bffa
Kamil284 <kkowalik5197@gmial.com>
2023-08-29 21:54:18 +0200

    Add break

--- Cenapaliwa1.py
+++ Cenapaliwa1.py
@@ -1,10 +1,13 @@
-def koszt_drogi(km,sp,cp): #km=ilosc kilometrow sp=spalanie na 100km cp=cena paliwa
+def koszt_drogi(km, sp, cp):  # km=ilosc kilometrow sp=spalanie na 100km cp=cena paliwa
     return km / sp * cp
-def ilosc_osob(km,sp,cp,os): ##km=ilosc kilometrow sp=spalanie na 100km cp=cena paliwa os=ilosc osob
+
+
+def ilosc_osob(km, sp, cp, os):  ##km=ilosc kilometrow sp=spalanie na 100km cp=cena paliwa os=ilosc osob
     return (km / sp * cp) / os
 
-while(True):
-    print('"Wybierz:\n"1" jeżeli jedziesz sam!\n"2" jeżeli jedziesz z kimś!')
+
+while (True):
+    print('"Wybierz:\n"1" jeżeli jedziesz sam!\n"2" jeżeli jedziesz z kimś!\n"STOP" aby zamknąć program')
     wybor = input("Wprowadz liczbe: ")
     if wybor == "1":
         km = float(input("Wprowadz ilość kilometrów do przejechania: "))
@@ -18,12 +21,16 @@ while(True):
         elif next.upper() == "NIE":
             print("Miłej i bezpiecznej drogi!")
             break
+        else:
+            print("Brak poprawnej odpowiedzi")
+            continue
     elif wybor == "2":
         km = float(input("Wprowadz ilość kilometrów do przejechania: "))
         sp = float(input("Wprowadz spalanie na 100km: "))
         cp = float(input("Wprowadz cene paliwa za jeden litr w PLN: "))
         os = float(input("Wprowadz liczbe osób w samochodzie: "))
-        print("Kosz przejchania tej trasy dla", int(os), "osób wynosi", round(ilosc_osob(km, sp, cp, os),2), "na osobe")
+        print("Kosz przejchania tej trasy dla", int(os), "osób wynosi", round(ilosc_osob(km, sp, cp, os), 2),
+              "na osobe")
         next = input("Czy chcesz wprowadzić dane jeszcze raz? TAK / NIE \n")
         if next.upper() == "TAK":
             print("Okej więc jeszcze raz!")
@@ -31,6 +38,8 @@ while(True):
         elif next.upper() == "NIE":
             print("Miłej i bezpiecznej drogi!")
             break
+    elif wybor.upper() == "STOP":
+        break
     else:
         print("Nie wybrałeś poprawnej opcji.\n" "Spróbuj ponownie!\n")
         continue

41d58f3f51a8263691c7e0a6898d9929c0307dd6
Kamil284 <kkowalik5197@gmial.com>
2023-09-15 20:56:51 +0200

    completed code

--- DaneUzytkownika.py
+++ DaneUzytkownika.py
@@ -11,8 +11,29 @@ def user_add():
     name = input('Name: ')
     surname = input('Surname: ')
     date_birth = input('Date of birth: ')
-    new_user = users(name, surname, date_birth)
+    new_user = User(name, surname, date_birth)
     users.append(new_user)
     print('User add completled.')
 
-def 
+def view_users():
+    print("Users: ")
+    for i, user in enumerate(users, start=1):
+        print(f"{i}. Name: {user.name}, Surname: {user.surname}, Year of birth: {user.date_birth}")
+
+while True:
+    print("Choice number: ")
+    print("1. User add")
+    print("2. View users")
+    print("3. Exit")
+
+    choice = input("Your choice: ")
+
+    if choice == "1":
+        user_add()
+    elif choice == "2":
+        view_users()
+    elif choice == "3":
+        print("Bye!")
+        break
+    else:
+        print("Your choice is wrong")

d22a173ccef3d9bbc808a541966b850b282cae38
Kamil284 <kkowalik5197@gmial.com>
2023-09-19 21:42:15 +0200

    Ready code

--- AutoryzachaWieku.py
+++ AutoryzachaWieku.py
@@ -4,7 +4,7 @@ def acces_age(year_of_birth):
     if min_age <= actually_year - year_of_birth:
         print("You are adult!")
     else:
-        print("You are not adult!")
+        print("You are not adult.")
 
 acces_age()
 
